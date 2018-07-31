570ad31f6e8f65cce0740409f177b29ad1aa88aa
Matthijs Alles <admin@bixie.nl>
2018-07-31 15:34:08 +0200

    export revenue invoices

--- app/components/invoice-accounting-entries.vue
+++ app/components/invoice-accounting-entries.vue
@@ -36,8 +36,7 @@
                 </div>
                 <ul class="uk-nestable uk-margin-remove" v-show="debitEntries.length">
                     <datafield v-for="datafield in debitEntries"
-                               :datafield="datafield"
-                               @check:balance="checkBalance"></datafield>
+                               :datafield="datafield"></datafield>
                 </ul>
 
                 <button type="button" class="uk-button uk-button-small uk-margin"
@@ -67,9 +66,6 @@ export default {
                     const ledger = _.find(this.$parent.ledger_numbers, {number,});
                     this.datafield.vat_code = ledger.vat_code;
                 },
-                'datafield.amount + datafield.vat_code'() {
-                    // this.$emit('check:balance');
-                },
             },
             template: '<li class="uk-nestable-item" :data-ledger_number="datafield.ledger_number">\n    <div class="uk-nestable-panel uk-visible-hover uk-form">\n        <div class="uk-margin-small-top uk-text-right">\n            <i class="uk-icon-euro uk-margin-small-right"></i>\n            <input type="number" class="uk-width-4-5 uk-text-right" v-model="datafield.amount" step="0.01" number/>\n        </div>\n        <div class="uk-grid uk-grid-small uk-margin-small-top">\n            <div class="uk-width-1-2">\n                <select v-model="datafield.ledger_number" class="uk-width-1-1 uk-form-small">\n                    <option value="">{{ \'Ledger number\' | trans }}</option>\n                    <option v-for="ledger_number in $parent.ledger_numbers"\n                            :value="ledger_number.number">{{ ledger_number.number }} - {{ ledger_number.label }}</option>\n                </select>\n            </div>\n            <div class="uk-width-1-2">\n                <select v-model="datafield.vat_code" class="uk-width-1-1 uk-form-small">\n                    <option value="">{{ \'No Vat Code\' | trans }}</option>\n                    <option v-for="vat_code in $parent.vat_codes" :value="vat_code.code">{{ vat_code.code }} - {{ vat_code.label }}</option>\n                </select>\n            </div>\n        </div>\n        <div class="uk-margin-small-top">\n            <input type="text" class="uk-width-1-1"\n                   v-model="datafield.description" :placeholder="$trans(\'Description\')"/>\n        </div>\n        <p class="uk-form-help-block uk-text-danger" v-show="datafield.invalid">{{ datafield.invalid | trans }}</p>\n        <div class="uk-margin-small-top">\n            <ul class="uk-subnav uk-flex-right pk-subnav-icon">\n                <li><a class="pk-icon-delete pk-icon-hover uk-invisible" @click="$parent.removeDatafield(datafield)"></a></li>\n            </ul>\n        </div>\n    </div>\n</li>   \n',
         },
@@ -91,9 +87,9 @@ export default {
         },
         balanceAmounts() {
             const addEntryTotal = (sum, entry) => {
-                const ledger = _.find(this.$parent.ledger_numbers, {number: entry.ledger_number,});
+                const vat_code = _.find(this.vat_codes, {code: entry.vat_code,});
                 let cent_amount = Math.round(entry.amount * 100);
-                if (ledger && ledger.vat_code) {
+                if (vat_code && vat_code.percentage) {
                     const vat_factor = 1 + (vat_code.percentage / 100);
                     cent_amount = Math.round(cent_amount * vat_factor)
                 }
@@ -106,8 +102,8 @@ export default {
                 debit,
                 credit,
                 rounded: {
-                    debit: Math.round(debit / 100),
-                    credit: Math.round(credit / 100),
+                    debit: debit / 100,
+                    credit: credit / 100,
                 },
             };
         },
