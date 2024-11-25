429033350da99950a7a2ec873a5587957103df2c
Jochem Schulten <jsn@schultenmedia.nl>
2024-11-26 00:48:32 +0100

    only allow ascii chars in filename

--- src/Controller/InvoiceApiController.php
+++ src/Controller/InvoiceApiController.php
@@ -2,6 +2,9 @@
 
 namespace Bixie\Invoicemaker\Controller;
 
+use Bixie\Freighthero\Calculator\CalculatorException;
+use Bixie\Freighthero\Helpers\CalculatorHelper;
+use Bixie\Freighthero\Model\CalculatorData;
 use Pagekit\Application as App;
 use Pagekit\Application\Exception;
 use Bixie\Invoicemaker\InvoicemakerModule;
@@ -17,56 +20,56 @@ use Symfony\Component\Routing\Generator\UrlGenerator;
  */
 class InvoiceApiController {
 
-	/**
-	 * @Route("/", methods="GET")
-	 * @Request({"filter": "array", "page":"int"})
-	 * @Access("invoicemaker: view own invoices")
-	 */
-	public function indexAction ($filter = [], $page = 0) {
-		$query = Invoice::query()->select('*, amount - amount_paid AS amount_open');
-		$filter = array_merge(array_fill_keys([
-		    'template', 'invoice_group', 'company_id', 'user_id', 'only', 'status', 'exported',
+    /**
+     * @Route("/", methods="GET")
+     * @Request({"filter": "array", "page":"int"})
+     * @Access("invoicemaker: view own invoices")
+     */
+    public function indexAction ($filter = [], $page = 0) {
+        $query = Invoice::query()->select('*, amount - amount_paid AS amount_open');
+        $filter = array_merge(array_fill_keys([
+            'template', 'invoice_group', 'company_id', 'user_id', 'only', 'status', 'exported',
             'search', 'ext_key', 'order', 'limit',
         ], ''), $filter);
 
-		extract($filter, EXTR_SKIP);
+        extract($filter, EXTR_SKIP);
 
-		$user = App::user();
-		if (!$user->hasAccess('invoicemaker: view invoices || invoicemaker: manage invoices')) {
-		    $user_id = $user->id;
+        $user = App::user();
+        if (!$user->hasAccess('invoicemaker: view invoices || invoicemaker: manage invoices')) {
+            $user_id = $user->id;
         }
 
-		if (!empty($template)) {
-			$query->where('template = :template', compact('template'));
-		}
+        if (!empty($template)) {
+            $query->where('template = :template', compact('template'));
+        }
 
-		if (!empty($invoice_group)) {
-			$query->where('invoice_group = :invoice_group', compact('invoice_group'));
-		}
+        if (!empty($invoice_group)) {
+            $query->where('invoice_group = :invoice_group', compact('invoice_group'));
+        }
 
-		if (!empty($ext_key)) {
-			$query->where('ext_key = :ext_key', compact('ext_key'));
-		}
+        if (!empty($ext_key)) {
+            $query->where('ext_key = :ext_key', compact('ext_key'));
+        }
 
-		if (!empty($user_id)) {
-			$query->where('user_id = :user_id', compact('user_id'));
-		}
+        if (!empty($user_id)) {
+            $query->where('user_id = :user_id', compact('user_id'));
+        }
 
-		if (!empty($company_id)) {
-			$query->where('company_id = :company_id', compact('company_id'));
-		}
+        if (!empty($company_id)) {
+            $query->where('company_id = :company_id', compact('company_id'));
+        }
 
-		if (!empty($status)) {
-			$query->where('status = :status', compact('status'));
-		}
+        if (!empty($status)) {
+            $query->where('status = :status', compact('status'));
+        }
 
         if (!empty($exported)) {
             $query->where(['exported' => $exported > 0]);
         }
 
         if (!empty($only_open)) {
-			$query->where('amount - amount_paid <> 0');
-		}
+            $query->where('amount - amount_paid <> 0');
+        }
 
         $query->where('deleted_at IS NULL');
 
@@ -77,34 +80,36 @@ class InvoiceApiController {
         }
 
         if (!preg_match('/^(invoice_number|ext_key|invoice_group|template|amount|amount_open|created)\s(asc|desc)$/i', $order, $order)) {
-			$order = [1 => 'invoice_number', 2 => 'desc'];
-		}
+            $order = [1 => 'invoice_number', 2 => 'desc'];
+        }
 
-		$limit = (int)$limit ?: 20;
-		$count = $query->count();
-		$pages = ceil($count / $limit);
-		$page = max(0, min($pages - 1, $page));
+        $limit = (int)$limit ?: 20;
+        $count = $query->count();
+        $pages = ceil($count / $limit);
+        $page = max(0, min($pages - 1, $page));
 
-		$invoices = array_values($query->offset($page * $limit)->limit($limit)->orderBy($order[1], $order[2])->get());
+        $invoices = array_values($query->offset($page * $limit)->limit($limit)->orderBy($order[1], $order[2])->get());
 
-		return compact('invoices', 'pages', 'count');
+        return compact('invoices', 'pages', 'count');
 
-	}
+    }
 
-	/**
-	 * @Route("/", methods="POST")
-	 * @Route("/{id}", methods="POST", requirements={"id"="\d+"})
-	 * @Request({"invoice": "array", "id": "int"}, csrf=true)
-	 * @Access("invoicemaker: manage invoices")
-	 */
-	public function saveAction ($data, $id = 0) {
+    /**
+     * @Route("/", methods="POST")
+     * @Route("/{id}", methods="POST", requirements={"id"="\d+"})
+     * @Request({"invoice": "array", "id": "int"}, csrf=true)
+     * @Access("invoicemaker: manage invoices")
+     */
+    public function saveAction ($data, $id = 0) {
 
-		if (!$invoice = Invoice::find($id)) {
-			$invoice = Invoice::create();
-			unset($data['id']);
-		}
+        if (!$invoice = Invoice::find($id)) {
+            $invoice = Invoice::create();
+            unset($data['id']);
+        }
 
-		try {
+        try {
+            // Check if owning shipment has changed.
+            $invoiceOwnerChanged = false;
 
             if(isset($data['shipment_id']) && strlen($data['shipment_id']) > 0) {
 
@@ -112,67 +117,67 @@ class InvoiceApiController {
                 unset($data['shipment_id']);
 
                 $shipment = Shipment::find($shipment_id);
-                if(!empty($shipment)) {
 
+                if(!empty($shipment) && strpos($data['ext_key'], 'tfh.shipment.') === false) {
                     $data['ext_key'] = 'tfh.shipment.' . $shipment_id;
+                    $invoiceOwnerChanged = true;
                 }
-
 
             }
 
-			$invoice->save($data);
+            $invoice->save($data);
 
-		} catch (Exception $e) {
-			App::abort(400, $e->getMessage());
-		}
+        } catch (Exception $e) {
+            App::abort(400, $e->getMessage());
+        }
 
-		return ['message' => 'success', 'invoice' => $invoice];
-	}
+        return ['message' => 'success', 'invoice' => $invoice];
+    }
 
-	/**
-	 * @Route("/{id}", methods="DELETE", requirements={"id"="\d+"})
-	 * @Request({"id": "int"}, csrf=true)
-	 * @Access("invoicemaker: manage invoices")
-	 */
-	public function deleteAction ($id) {
-		if ($invoice = Invoice::find($id)) {
+    /**
+     * @Route("/{id}", methods="DELETE", requirements={"id"="\d+"})
+     * @Request({"id": "int"}, csrf=true)
+     * @Access("invoicemaker: manage invoices")
+     */
+    public function deleteAction ($id) {
+        if ($invoice = Invoice::find($id)) {
 
             if($invoice->exported === true) {
                 return ['message' => 'error'];
             }
             $invoice->delete();
 
-		}
+        }
 
-		return ['message' => 'success'];
-	}
+        return ['message' => 'success'];
+    }
 
-	/**
-	 * @Route("/bulk", methods="DELETE")
-	 * @Request({"ids": "array"}, csrf=true)
-	 * @Access("invoicemaker: manage invoices")
-	 */
-	public function bulkDeleteAction ($ids = []) {
-		foreach (array_filter($ids) as $id) {
-			$result = $this->deleteAction($id);
+    /**
+     * @Route("/bulk", methods="DELETE")
+     * @Request({"ids": "array"}, csrf=true)
+     * @Access("invoicemaker: manage invoices")
+     */
+    public function bulkDeleteAction ($ids = []) {
+        foreach (array_filter($ids) as $id) {
+            $result = $this->deleteAction($id);
             if($result['message'] == 'error') return $result;
-		}
+        }
 
-		return ['message' => 'success'];
-	}
+        return ['message' => 'success'];
+    }
 
-	/**
-	 * @Route("/credit/{id}", methods="POST", name="credit")
-	 * @Request({"id": "integer"}, csrf=true)
-	 * @Access("invoicemaker: manage invoices")
-	 */
-	public function creditAction($id) {
-		/** @var InvoicemakerModule $invoicemaker */
-		$invoicemaker = App::module('bixie/invoicemaker');
+    /**
+     * @Route("/credit/{id}", methods="POST", name="credit")
+     * @Request({"id": "integer"}, csrf=true)
+     * @Access("invoicemaker: manage invoices")
+     */
+    public function creditAction($id) {
+        /** @var InvoicemakerModule $invoicemaker */
+        $invoicemaker = App::module('bixie/invoicemaker');
 
-		if (!$invoice = Invoice::find($id)) {
-			App::abort(404, __('Invoice not found'));
-		}
+        if (!$invoice = Invoice::find($id)) {
+            App::abort(404, __('Invoice not found'));
+        }
         try {
 
             if ($credit_invoice = $invoicemaker->creditInvoice($invoice->invoice_number)) {
@@ -188,103 +193,103 @@ class InvoiceApiController {
             App::abort(500, __('Error in creating Credit invoice'));
         }
 
-	}
+    }
 
-	/**
-	 * @Route("/rerender/{id}", name="rerender")
-	 * @Request({"id": "integer"})
-	 * @Access("invoicemaker: manage invoices")
-	 */
-	public function reRenderPdfAction($id) {
-		/** @var InvoicemakerModule $invoicemaker */
-		$invoicemaker = App::module('bixie/invoicemaker');
+    /**
+     * @Route("/rerender/{id}", name="rerender")
+     * @Request({"id": "integer"})
+     * @Access("invoicemaker: manage invoices")
+     */
+    public function reRenderPdfAction($id) {
+        /** @var InvoicemakerModule $invoicemaker */
+        $invoicemaker = App::module('bixie/invoicemaker');
 
-		if (!$invoice = Invoice::find($id)) {
-			App::abort(404, __('Invoice not found'));
-		}
+        if (!$invoice = Invoice::find($id)) {
+            App::abort(404, __('Invoice not found'));
+        }
 
-		try {
+        try {
 
-			if ($invoicemaker->renderPdfFile($invoice)) {
+            if ($invoicemaker->renderPdfFile($invoice)) {
 
-				$invoice->save([
-					'pdf_file' => $invoice->getPdfFilename()
-				]);
+                $invoice->save([
+                    'pdf_file' => $invoice->getPdfFilename()
+                ]);
 
-			}
+            }
 
-		} catch (\Exception $e) {
-			App::abort(500, __('Error in creating PDF file'));
-		}
+        } catch (\Exception $e) {
+            App::abort(500, __('Error in creating PDF file'));
+        }
 
-		return ['message' => 'success'];
-	}
+        return ['message' => 'success'];
+    }
 
-	/**
-	 * @Route("/pdf/{invoice_number}", name="pdf")
-	 * @Request({"invoice_number": "string", "key": "string", "inline": "bool"})
-	 * @Access("invoicemaker: view own invoices")
-	 * @param integer $invoice_number Invoice bumber
-	 * @param string  $key            session key
-	 * @param bool    $inline
-	 * @return StreamedResponse|BinaryFileResponse
-	 */
-	public function pdfAction($invoice_number, $key, $inline = false) {
-		/** @var InvoicemakerModule $invoicemaker */
-		$invoicemaker = App::module('bixie/invoicemaker');
+    /**
+     * @Route("/pdf/{invoice_number}", name="pdf")
+     * @Request({"invoice_number": "string", "key": "string", "inline": "bool"})
+     * @Access("invoicemaker: view own invoices")
+     * @param integer $invoice_number Invoice bumber
+     * @param string  $key            session key
+     * @param bool    $inline
+     * @return StreamedResponse|BinaryFileResponse
+     */
+    public function pdfAction($invoice_number, $key, $inline = false) {
+        /** @var InvoicemakerModule $invoicemaker */
+        $invoicemaker = App::module('bixie/invoicemaker');
 
-		if (!$invoice = Invoice::byInvoiceNumber($invoice_number)) {
-			App::abort(404, __('Invoice not found'));
-		}
+        if (!$invoice = Invoice::byInvoiceNumber($invoice_number)) {
+            App::abort(404, __('Invoice not found'));
+        }
 
-		if (!$invoicemaker->checkDownloadKey($invoice, $key)) {
-			App::abort(403, __('Key not valid.'));
-		}
+        if (!$invoicemaker->checkDownloadKey($invoice, $key)) {
+            App::abort(403, __('Key not valid.'));
+        }
 
-		if ($filename = $invoice->pdf_file and $path = $invoicemaker->getPdfPath() . '/' . $filename) {
-			//existing file
-			$response = new BinaryFileResponse($path);
+        if ($filename = $invoice->pdf_file and $path = $invoicemaker->getPdfPath() . '/' . $filename) {
+            //existing file
+            $response = new BinaryFileResponse($path);
 
-		} else {
-			//generate stream
-			$filename = $invoice->getPdfFilename();
-			$response = new StreamedResponse();
-			$response->setCallback(function () use ($invoicemaker, $invoice) {
-				echo $invoicemaker->renderPdfString($invoice);
-			});
-			$response->setStatusCode(200);
-			$response->headers->set('Content-Type', 'application/pdf; charset=utf-8');
+        } else {
+            //generate stream
+            $filename = $invoice->getPdfFilename();
+            $response = new StreamedResponse();
+            $response->setCallback(function () use ($invoicemaker, $invoice) {
+                echo $invoicemaker->renderPdfString($invoice);
+            });
+            $response->setStatusCode(200);
+            $response->headers->set('Content-Type', 'application/pdf; charset=utf-8');
 
-		}
+        }
 
-		$response->headers->set('Content-Disposition', $response->headers->makeDisposition(
-			($inline ? ResponseHeaderBag::DISPOSITION_INLINE: ResponseHeaderBag::DISPOSITION_ATTACHMENT),
-			$filename,
-			mb_convert_encoding($filename, 'ASCII')
-		));
+        $response->headers->set('Content-Disposition', $response->headers->makeDisposition(
+            ($inline ? ResponseHeaderBag::DISPOSITION_INLINE: ResponseHeaderBag::DISPOSITION_ATTACHMENT),
+            $filename,
+            mb_convert_encoding($filename, 'ASCII')
+        ));
 
-		return $response;
+        return $response;
 
-	}
+    }
 
-	/**
-	 * @Route("/html/{invoice_number}", name="html")
-	 * @Request({"invoice_number": "string", "key": "string"})
-	 * @param integer $invoice_number Invoice bumber
-	 * @param string  $key            session key
-	 * @return StreamedResponse
-	 */
-	public function htmlAction($invoice_number, $key) {
-		/** @var InvoicemakerModule $invoicemaker */
-		$invoicemaker = App::module('bixie/invoicemaker');
+    /**
+     * @Route("/html/{invoice_number}", name="html")
+     * @Request({"invoice_number": "string", "key": "string"})
+     * @param integer $invoice_number Invoice bumber
+     * @param string  $key            session key
+     * @return StreamedResponse
+     */
+    public function htmlAction($invoice_number, $key) {
+        /** @var InvoicemakerModule $invoicemaker */
+        $invoicemaker = App::module('bixie/invoicemaker');
 
-		if (!$invoice = Invoice::byInvoiceNumber($invoice_number)) {
-			App::abort(404, __('Invoice not found'));
-		}
+        if (!$invoice = Invoice::byInvoiceNumber($invoice_number)) {
+            App::abort(404, __('Invoice not found'));
+        }
 
-		if (!$invoicemaker->checkDownloadKey($invoice, $key)) {
-			App::abort(400, __('Key not valid.'));
-		}
+        if (!$invoicemaker->checkDownloadKey($invoice, $key)) {
+            App::abort(400, __('Key not valid.'));
+        }
 
 //        try {
 //
@@ -301,22 +306,30 @@ class InvoiceApiController {
 //            App::abort(500, __('Error in creating PDF file'));
 //        }
 
-		$filename = $invoice->getPdfFilename();
+        $filename = $invoice->getPdfFilename();
 
-		$response = new StreamedResponse();
-		$response->setCallback(function () use ($invoicemaker, $invoice) {
-			echo str_replace(App::path(), App::url()->getStatic('/', [], UrlGenerator::ABSOLUTE_URL), $invoicemaker->renderHtml($invoice));
-		});
-		$response->setStatusCode(200);
-		$response->headers->set('Content-Type', 'text/html; charset=utf-8');
-		$response->headers->set('Content-Disposition', $response->headers->makeDisposition(
-			ResponseHeaderBag::DISPOSITION_INLINE,
-			$filename,
-			mb_convert_encoding($filename, 'ASCII')
-		));
+// Remove non-ASCII characters
+        $asciiFilename = preg_replace('/[^\x20-\x7E]/', '', $filename);
 
-		return $response;
+// Optionally, you could log or handle cases where characters are removed
+        if ($asciiFilename !== $filename) {
+            $filename = $asciiFilename;
+        }
 
-	}
+        $response = new StreamedResponse();
+        $response->setCallback(function () use ($invoicemaker, $invoice) {
+            echo str_replace(App::path(), App::url()->getStatic('/', [], UrlGenerator::ABSOLUTE_URL), $invoicemaker->renderHtml($invoice));
+        });
+        $response->setStatusCode(200);
+        $response->headers->set('Content-Type', 'text/html; charset=utf-8');
+        $response->headers->set('Content-Disposition', $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_INLINE,
+            $filename,
+            mb_convert_encoding($filename, 'ASCII')
+        ));
+
+        return $response;
+
+    }
 
 }
