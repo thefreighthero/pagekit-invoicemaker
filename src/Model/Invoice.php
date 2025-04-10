edfd29b1e1a2ef8829eec2d18225d135453fe9af
Nikita Skliarov <0357026@student.rocvantwente.nl>
2025-04-10 14:24:11 +0200

    Add new property to model

--- src/Model/Invoice.php
+++ src/Model/Invoice.php
@@ -16,6 +16,11 @@ class Invoice implements \JsonSerializable {
 
 	use DataModelTrait, InvoiceModelTrait;
 
+    /* Invoice booking types: REPEAT, NEW, YEAR BOOKING */
+    const BOOKING_TYPE_REPEAT = "REPEAT";
+    const BOOKING_TYPE_NEW  = "NEW";
+    const BOOKING_TYPE_YEAR_BOOKING = "YEAR_BOOKING";
+
     /* Invoice initial and valid. */
     const STATUS_INITIAL = 'INITIAL';
 
@@ -98,6 +103,13 @@ class Invoice implements \JsonSerializable {
      * @var \DateTime
      */
     public $exported_at;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    public $booking_type;
+
     /**
 	 * @Column(type="string")
 	 * @var string
